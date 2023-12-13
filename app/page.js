a4d8ef7db9c5542fd0f592da40f9abc14c57d56f
12OtherWays <reeturajsinghaniya31@gmail.com>
2023-12-13 19:21:22 +0530

    added font family

--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "./utils/Theme";
-import Button from "@mui/material/Button";
 
 export default function Home() {
-  return (
-    <ThemeProvider theme={Theme}>
-      <Button>shdfhl</Button>
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={Theme}></ThemeProvider>;
 }
