dfa6fbe3ed5be05edd93ba091fc0eae1f0eb9fa4
12OtherWays <reeturajsinghaniya31@gmail.com>
2023-12-14 17:21:02 +0530

    added first bar chart

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "./utils/Theme";
-import Button from "@mui/material/Button";
+import { BarGraph } from "./component/graph/charts/BarGraph";
+import InputLayout from "./component/input/InputLayout";
+import GraphLayout from "./component/graph/GraphLayout";
 
 export default function Home() {
   return (
     <ThemeProvider theme={Theme}>
-      <Button>shdfhl</Button>
+      <InputLayout />
+      <GraphLayout />
     </ThemeProvider>
   );
 }
