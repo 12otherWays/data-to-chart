17eb0f3ad7bd547eb529dcc5836fa590ea1ab39d
12OtherWays <reeturajsinghaniya31@gmail.com>
2023-12-14 17:21:02 +0530

    added first bar chart

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+"use client";
 import { ThemeProvider } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
 import getDesignTokens from "./utils/Theme";
-const Theme = createTheme(getDesignTokens("dark"));
+
+const Theme = createTheme(getDesignTokens("light"));
 
 export default function Home() {
   return <ThemeProvider theme={Theme}></ThemeProvider>;
